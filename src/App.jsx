f9709927a431e36547685aebbc46122d9da4aa41
Herve Desrosiers <hdesrosiers@stingray.com>
2025-12-18 15:05:15 -0500

    Button - props review challenge

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+// import { useState } from "react";
+import "./App.css";
+import Button from "./Button";
+
+/**
+ * Challenge:
+ *
+ * Build a Button component in a separate file.
+ * (For styling purposes, make sure it renders an HTML <button> element)
+ * Your button component should take a `text` prop
+ */
+
+function App() {
+  return (
+    <main>
+      <Button text="Click me" />
+    </main>
+  );
+}
+
+export default App;
