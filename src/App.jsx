46f1db86d8905fccba6b75cc5fa490cffc56237e
Herve Desrosiers <hdesrosiers@stingray.com>
2025-12-19 07:30:59 -0500

    Challenge - add onClick event listener @ https://scrimba.com/advanced-react-c02h/~07

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,21 @@ import Button from "./Button";
 import { FaMoneyBill } from "react-icons/fa6";
 
 /**
- * Challenge: Add the "FaMoneyBill" icon to the left
- * of the "Buy now!" text in the button
+ * Challenge: Add an onClick handler to the Button
+ * component instance (the one on this page). Then
+ * make whatever changes you need for it to actually
+ * work.
+ *
+ * Clicking the button should log "Logging in..."
+ * to the console.
  */
+
+const handleClick = () => console.log("Logging in...");
 
 function App() {
   return (
     <main>
-      <Button>
+      <Button onClick={handleClick}>
         <FaMoneyBill />
         Buy now!
       </Button>
