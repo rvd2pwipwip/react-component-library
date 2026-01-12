c671a7bc324fc878c792efae40cff92688e4250c
Herve Desrosiers <hdesrosiers@stingray.com>
2026-01-12 11:24:02 -0500

    Remove Star component @ https://scrimba.com/advanced-react-c02h/~018

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,24 @@ import "./App.css";
 import Button from "./Components/Button/Button";
 import Avatar from "./Components/Avatar";
 import Menu from "./Components/Menu/index";
-import Star from "./Components/Star";
 import Toggle from "./Toggle/index";
 import { FaMoneyBill } from "react-icons/fa6";
+import { BsStar, BsStarFill } from "react-icons/bs";
 
 // const handleClick = () => console.log("Logging in...");
+
+/**
+ * Challenge: refactor our app to use the Toggle.On
+ * and Toggle.Off components to conditionally render the
+ * star icons from react-icons. (See Star.js for that code).
+ *
+ * Reminders:
+ * 1. Make sure to copy over the classNames from Star.js too!
+ *    I've added some CSS that we don't want to lose.
+ * 2. Clicking the Star should flip the Toggle state. Try to
+ *    figure out how you can do that 🤔. Hint: we built something
+ *    that lets us flip the Toggle state back and forth 😉
+ */
 
 function App() {
   const sports = ["Tennis", "Pickleball", "Racquetball", "Squash"];
@@ -26,10 +39,13 @@ function App() {
       {/* <Avatar /> */}
       <Toggle>
         <Toggle.Button>
-          <Star />
+          <Toggle.On>
+            <BsStarFill className="star filled" />
+          </Toggle.On>
+          <Toggle.Off>
+            <BsStar className="star " />
+          </Toggle.Off>
         </Toggle.Button>
-        <Toggle.On>The toggle is on</Toggle.On>
-        <Toggle.Off>The toggle is off</Toggle.Off>
       </Toggle>
       <br />
       <Menu>
