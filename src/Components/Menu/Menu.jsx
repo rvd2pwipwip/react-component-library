f6967ced2557f587fe3df7542ab898124f3dbe7d
Herve Desrosiers <hdesrosiers@stingray.com>
2026-01-13 10:41:49 -0500

    Fix onToggle bug using refs @ https://scrimba.com/advanced-react-c02h/~01f

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,15 +1,6 @@
 import React from "react";
 import Toggle from "../../Toggle/index";
 
-/**
- * Challenge:
- * 1. Accept `onOpen` as a prop for the Menu component
- * 2. Pass the onOpen prop as the value to Toggle's onToggle prop
- * 3. In index.js, pass an onOpen prop to the Menu
- *    component whose value is a function.
- *    Just use console.log() for now.
- */
-
 const MenuContext = React.createContext();
 
 const Menu = ({ children, onOpen }) => {
--- src/Toggle/Toggle.jsx
+++ src/Toggle/Toggle.jsx
@@ -1,5 +1,21 @@
 import React from "react";
 
+/**
+ * Challenge: We only want to run onToggle() AFTER the
+ * first time rendering the component. We can use refs to
+ * track whether or not it's the first time this component
+ * is rendering
+ *
+ * 1. Create a ref called `firstRender` which defaults to `true`
+ *    (Since when we first create the ref, it's true that it
+ *    is the first time the component is rendering)
+ * 2. In the useEffect, check if your ref's value is `true`.
+ *    If it is, set it to `false`. If it isn't... can you figure
+ *    it out??
+ * Hint: don't forget that your boolean value will be saved
+ * under `firstRender.current`, not just `firstRender`!
+ */
+
 const ToggleContext = React.createContext();
 
 const Toggle = ({ children, onToggle }) => {
@@ -9,8 +25,10 @@ const Toggle = ({ children, onToggle }) => {
     setBoolean((prevBool) => !prevBool);
   };
 
+  const firstRender = React.useRef(true);
+
   React.useEffect(() => {
-    onToggle();
+    firstRender.current ? (firstRender.current = false) : onToggle();
   }, [boolean]);
 
   return (
