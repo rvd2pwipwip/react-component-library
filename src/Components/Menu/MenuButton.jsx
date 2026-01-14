8b1b2ad02c11858db25bce749cbbfb1abe0fc333
Herve Desrosiers <hdesrosiers@stingray.com>
2026-01-14 15:02:22 -0500

    Custom Hooks - useToggle part 6 @ https://scrimba.com/advanced-react-c02h/~01u and moved onClick event from parent div to MenuButton

--- src/Components/Menu/MenuButton.jsx
+++ src/Components/Menu/MenuButton.jsx
@@ -2,11 +2,26 @@ import React from "react";
 import Button from "../Button/Button";
 import { MenuContext } from "./Menu";
 
+/**
+ * Challenge:
+ * 6. Access the needed context values in the MenuButton
+ *    and MenuDropdown components. See if you can remember
+ *    (or figure out again) how those values are used in each
+ *    of those components. Once you've done this successfully,
+ *    the Menu should start working again (except for the onOpen
+ *    callback, which we'll work on fixing soon).
+ */
+
 const MenuButton = ({ children }) => {
-  const { open, menuId } = React.useContext(MenuContext);
+  const { open, toggleOpen, menuId } = React.useContext(MenuContext);
 
   return (
-    <Button aria-expanded={open} aria-haspopup="true" aria-controls={menuId}>
+    <Button
+      onClick={toggleOpen}
+      aria-expanded={open}
+      aria-haspopup="true"
+      aria-controls={menuId}
+    >
       {children}
     </Button>
   );
