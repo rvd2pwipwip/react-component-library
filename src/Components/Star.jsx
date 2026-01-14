a6bc0e269bab5c6fb97d67328a2ec1c93e06a5a1
Herve Desrosiers <hdesrosiers@stingray.com>
2026-01-14 13:47:50 -0500

    Custom Hooks - useToggle part 3 @ https://scrimba.com/advanced-react-c02h/~01r

--- src/Components/Star.jsx
+++ src/Components/Star.jsx
@@ -1,18 +1,25 @@
-import Toggle from "./Toggle/index";
 import { BsStar, BsStarFill } from "react-icons/bs";
+import useToggle from "./Hooks/useToggle";
 
-const Star = ({ onChange }) => {
+/**
+ * Challenge: Remove all parts of our Toggle component
+ * (don't delete the react-icons stars though!) and use
+ * the useToggle hook to conditionally render the stars.
+ * Make sure to add an onClick that runs the `toggle` function
+ * you get from useToggle()
+ */
+
+const Star = () => {
+  const [boolean, toggle] = useToggle();
+
   return (
-    <Toggle onToggle={onChange}>
-      <Toggle.Button>
-        <Toggle.On>
-          <BsStarFill className="star filled" />
-        </Toggle.On>
-        <Toggle.Off>
-          <BsStar className="star " />
-        </Toggle.Off>
-      </Toggle.Button>
-    </Toggle>
+    <div onClick={toggle}>
+      {boolean ? (
+        <BsStarFill className="star filled" />
+      ) : (
+        <BsStar className="star " />
+      )}
+    </div>
   );
 };
 
