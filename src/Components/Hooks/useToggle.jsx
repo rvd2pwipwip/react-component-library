914a197ca56e7603545f1e65d6e2fc658f5a20a9
Herve Desrosiers <hdesrosiers@stingray.com>
2026-01-15 08:18:28 -0500

    Custom Hooks - useToggle part 7 @ https://scrimba.com/advanced-react-c02h/~01v

--- src/Components/Hooks/useToggle.jsx
+++ src/Components/Hooks/useToggle.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const useToggle = () => {
-  const [bool, setBool] = React.useState(false);
+/**
+ * Challenge:
+ * 1. Pass a parameter called `initialValue` to our custom hook.
+ *    Have its default be `false` in case that parameter isn't
+ *    provided when useToggle() is called.
+ * 2. Initialize state with the `initialValue` parameter
+ */
+
+const useToggle = (initialValue = false) => {
+  const [bool, setBool] = React.useState(initialValue);
 
   const toggle = () => {
     setBool((prevBool) => !prevBool);
--- src/Components/Menu/MenuButton.jsx
+++ src/Components/Menu/MenuButton.jsx
@@ -2,16 +2,6 @@ import React from "react";
 import Button from "../Button/Button";
 import { MenuContext } from "./Menu";
 
-/**
- * Challenge:
- * 6. Access the needed context values in the MenuButton
- *    and MenuDropdown components. See if you can remember
- *    (or figure out again) how those values are used in each
- *    of those components. Once you've done this successfully,
- *    the Menu should start working again (except for the onOpen
- *    callback, which we'll work on fixing soon).
- */
-
 const MenuButton = ({ children }) => {
   const { open, toggleOpen, menuId } = React.useContext(MenuContext);
 
